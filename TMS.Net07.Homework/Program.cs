8e8d5e8bc7b94a9b3547ea3ea8a98d615182997c
Aleksander <Greengow@mail.ru>
2021-01-31 18:39:30 +0300

    initial

---
+++ TMS.Net07.Homework/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TMS.Net07.Homework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = Console.ReadLine(); ;
+            int b;
+            {
+
+                if (int.TryParse(a, out b))
+                {
+                    Console.Clear();
+                    Console.WriteLine($"Введено число {b}");
+                }
+                else
+                {
+                    Console.Clear();
+                    Console.WriteLine("Это не число");
+                }
+                Console.ReadLine();
+            }
+        }
+    }
+}
